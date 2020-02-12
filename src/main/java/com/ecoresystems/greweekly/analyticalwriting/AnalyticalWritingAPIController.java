fb30aed56413b0ab9234c89770257b766d6d6e16
Sonoda Umi <det_conan@live.cn>
2020-02-12 16:49:29 +0900

    Add some response

--- src/main/java/com/ecoresystems/greweekly/analyticalwriting/AnalyticalWritingAPIController.java
+++ src/main/java/com/ecoresystems/greweekly/analyticalwriting/AnalyticalWritingAPIController.java
@@ -5,13 +5,18 @@ import com.ecoresystems.greweekly.analyticalwriting.domain.WritingAnswer;
 import com.ecoresystems.greweekly.analyticalwriting.service.AnalyticalWritingService;
 import com.ecoresystems.greweekly.auth.User;
 import com.ecoresystems.greweekly.auth.UserRepository;
+import com.ecoresystems.greweekly.data.entity.AnalyticalWriting;
 import com.ecoresystems.greweekly.data.repository.WritingAnswersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.UnsupportedEncodingException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value="/api")
@@ -39,9 +44,14 @@ public class AnalyticalWritingAPIController {
     }
 
     @GetMapping(value = "/get_answers")
-    public @ResponseBody List<WritingAnswer> getAnswers(Authentication authentication) throws UnsupportedEncodingException{
+    public ResponseEntity<Map<String, Object>> getAnswers(Authentication authentication) throws UnsupportedEncodingException{
 //        String userMail = authentication.getName();
+        Map<String,Object> response = new HashMap<>();
         String userMail = "sample@sample.com";
-        return analyticalWritingService.getWritingAnswersForUser(userMail);
+        List<WritingAnswer> writingAnswers = analyticalWritingService.getWritingAnswersForUser(userMail);
+        List<AnalyticalWriting> writingQuestions = analyticalWritingService.getAllWritingQuestions();
+        response.put("answers",writingAnswers);
+        response.put("questions",writingQuestions);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
